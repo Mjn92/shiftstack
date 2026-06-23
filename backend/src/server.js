134db394f8bdeb81e45890b6c09a8d34ee9c9d5a
Matthew Nicol <mjnicol92@gmail.com>
2026-06-23 13:54:38 -0400

    updated web dashbord

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,11 @@ const { connectRabbitMQ } = require("./config/rabbitmq");
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_URL_2,
+].filter(Boolean);
 
 app.use(
   cors({
